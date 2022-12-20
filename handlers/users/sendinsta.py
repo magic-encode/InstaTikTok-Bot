f0db0a91de53c0351469338e656b10c332ad7ffc
Abdullajon <101052604+django-dev-uz@users.noreply.github.com>
2022-12-20 11:53:22 +0500

    Fixed another files

--- handlers/users/sendinsta.py
+++ handlers/users/sendinsta.py
@@ -8,23 +8,30 @@ from aiogram.dispatcher.filters import Text
 
 @dp.message_handler(Text(startswith='https://www.instagram.com/'))
 async def send_media(message:types.Message):
-    link = message.text
-    data = instadown(link=link)
-    if data == 'Tashlagan linkda xatolik bor...':
-        await  message.answer('Iltimos boshqa link tashlang, bu link ishlamaydi..')
-    else:
-        if data['type'] == 'image':
-            await message.answer_photo(photo=data['media'])
-        elif data['type'] == 'video':
-            await message.answer_video(video=data['media'])
-            
-        elif data['type'] == 'carousel':
-            for i in data['media']:
-                await message.answer_document(document=i)
-        elif data['type'] == 'story-video':
-            for i in data['media']:
-                await message.answer_document(document=i)
-        else:
+    
+    try:
+        link = message.text
+        data = instadown(link=link)
+        if data == 'Tashlagan linkda xatolik bor...':
             await  message.answer('Iltimos boshqa link tashlang, bu link ishlamaydi..')
+    
+        else:
+            if data['type'] == 'image':
+                await message.answer_photo(photo=data['media'])
+            elif data['type'] == 'video':
+                await message.answer_video(video=data['media'])
+                
+            elif data['type'] == 'carousel':
+                for i in data['media']:
+                    await message.answer_document(document=i)
+            elif data['type'] == 'story-video':
+                for i in data['media']:
+                    await message.answer_document(document=i)
+            else:
+                await  message.answer('Iltimos boshqa link tashlang, bu link ishlamaydi..')
+    except:
+        await  message.answer('Iltimos boshqa link tashlang video hajmi yuqori..')
+                    
+    
             
         
