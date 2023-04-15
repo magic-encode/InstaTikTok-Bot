d613b6fe0087749e7a4cf1fc884636a7e7a4f1b9
Abdullajon <101052604+django-dev-uz@users.noreply.github.com>
2023-04-15 17:45:07 +0500

    Fixed youtube section

--- handlers/users/youtube.py
+++ handlers/users/youtube.py
@@ -1,53 +1,23 @@
-# from aiogram import types
-# from loader import dp
-
-
-# from pytube import YouTube
-# from aiogram.dispatcher.filters import Text
-
-# @dp.message_handler(Text(startswith="http"))
-# async def get_audio(message: types.Message):
-#     link=message.text
-#     from io import BytesIO
-#     buffer=BytesIO()
-#     url=YouTube(link)
-#     if url.check_availability() is None:
-#         audio=url.streams.get_audio_only()
-#         audio.stream_to_buffer(buffer=buffer)
-#         buffer.seek(0)
-#         filename=url.title
-#         await message.answer_audio(audio=buffer, caption=filename)
-#     else:
-#         await message.answer('Xatolik')
-        
-        
 import logging
+from loader import dp
 from io import BytesIO
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import types
 from pytube import YouTube
-
-API_TOKEN = '5472463935:AAGLQHHdLtLJEAYRg_PttD_DRhKT64vS1oI'
+from aiogram.dispatcher.filters import Text
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
 
-from aiogram.dispatcher.filters import Text
-
-@dp.message_handler(Text(startswith="http"))
+@dp.message_handler(Text(startswith="https://youtu.be/"))
 async def get_audio(message: types.Message):
-    link=message.text
-    buffer=BytesIO()
-    url=YouTube(link)
+    link = message.text
+    buffer = BytesIO()
+    url = YouTube(link)
     if url.check_availability() is None:
-        audio=url.streams.get_audio_only()
+        audio = url.streams.get_audio_only()
         audio.stream_to_buffer(buffer=buffer)
         buffer.seek(0)
-        filename=url.title
+        filename = url.title
         await message.answer_audio(audio=buffer, caption=filename)
     else:
         await message.answer('Xatolik')
-
-if __name__ == '__main__':
-    executor.start_polling(dp)
--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -5,7 +5,5 @@ async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
             types.BotCommand("start", "Botni ishga tushurish"),
-            types.BotCommand("help", "Yordam"),
-            types.BotCommand("settings", "Sozlamar"),
         ]
     )
